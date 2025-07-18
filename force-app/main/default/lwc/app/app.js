3fa1711825506ada06952a793db9f16c902a1bd7
Thapelo <thapelo.seema@gmail.com>
2025-07-18 13:56:21 +0200

    changed the test class

--- force-app/main/default/lwc/app/app.js
+++ force-app/main/default/lwc/app/app.js
@@ -1,15 +1,25 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api, track } from 'lwc';
+import getAccounts  from '@salesforce/apex/TestClass.getAccounts';
+ 
 export default class App extends LightningElement {
   name = 'Electra X4';
+  @track names = ['thapelo', 'semma', 'david'];
   description = 'A sweet bike built for comfort.';
-  category = 'Mountain';
-  material = 'Steel';
+  @api category = 'Mountain';
+  @api material = 'Steel';
   price = '$2,700';
   pictureUrl = 'https://s3-us-west-2.amazonaws.com/dev-or-devrl-s3-bucket/sample-apps/ebikes/electrax4.jpg';
   ready = false;
+
   connectedCallback() {
     setTimeout(() => {
       this.ready = true;
     }, 3000);
   }
+
+  callApex(){
+    this.material = 'wool';
+    getAccounts();
+  }
+
 }
